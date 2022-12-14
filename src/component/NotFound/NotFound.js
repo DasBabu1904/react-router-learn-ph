377b88d4936734f3a39b22bb982454e05a0ae653
Dass7550 <dass7550@gmail.com>
2022-12-14 14:26:10 +0600

    React Router key word used for first time

---
+++ src/component/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3>This is not found section </h3>
+            <h4>404</h4>
+        </div>
+    );
+};
+
+export default NotFound;
