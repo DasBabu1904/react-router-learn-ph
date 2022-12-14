4c6dcee6782684ddc2b022436452aff3bc374e37
Dass7550 <dass7550@gmail.com>
2022-12-14 15:54:08 +0600

    active style improved

--- src/component/CustomLink/CustomLink.js
+++ src/component/CustomLink/CustomLink.js
@@ -8,13 +8,13 @@ function CustomLink({ children, to, ...props }) {
     return (
         <div>
             <Link
-                style={{ textDecoration: match ? "underline" : "none" }}
+                style={{ color: match ? "red" : "black", textDecoration: match ? "underline" : "none" }}
                 to={to}
                 {...props}
             >
                 {children}
             </Link>
-            {match && " (active)"}
+
         </div>
     );
 }
